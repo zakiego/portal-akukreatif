ca7fb507d7d13c76bc0d3a4e5f94169f83b11c44
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-09-18 20:03:42 +0800

    feat: move description above and remove section additional details

--- src/app/profil/[id]/client.tsx
+++ src/app/profil/[id]/client.tsx
@@ -105,18 +105,6 @@ export default function ProductClient({ data }: Props) {
               </div>
             </div> */}
 
-            {/* DESCRIPTION */}
-            <div className="mt-6">
-              <h3 className="sr-only">Deskripsi</h3>
-
-              <div
-                className="space-y-6 text-base text-gray-700"
-                dangerouslySetInnerHTML={{
-                  __html: data.attributes.description || "",
-                }}
-              />
-            </div>
-
             {/* DETAILS */}
 
             <div className="mt-6 border-t border-gray-100">
@@ -170,6 +158,18 @@ export default function ProductClient({ data }: Props) {
                   );
                 })}
               </dl>
+            </div>
+
+            {/* DESCRIPTION */}
+            <div className="mt-6">
+              <h3 className="sr-only">Deskripsi</h3>
+
+              <div
+                className="space-y-6 text-base text-gray-700"
+                dangerouslySetInnerHTML={{
+                  __html: data.attributes.description || "",
+                }}
+              />
             </div>
 
             <form className="mt-6">
@@ -235,59 +235,6 @@ export default function ProductClient({ data }: Props) {
                 </button>
               </div> */}
             </form>
-
-            <section aria-labelledby="details-heading" className="mt-12">
-              <h2 id="details-heading" className="sr-only">
-                Additional details
-              </h2>
-
-              <div className="divide-y divide-gray-200 border-t">
-                {[
-                  {
-                    name: "Hubungi Kami",
-                    items: <div>{data.attributes.address}</div>,
-                  },
-                ].map((detail) => (
-                  <Disclosure as="div" key={detail.name}>
-                    {({ open }) => (
-                      <>
-                        <h3>
-                          <Disclosure.Button className="group relative flex w-full items-center justify-between py-6 text-left">
-                            <span
-                              className={classNames(
-                                open ? "text-indigo-600" : "text-gray-900",
-                                "text-sm font-medium",
-                              )}
-                            >
-                              {detail.name}
-                            </span>
-                            <span className="ml-6 flex items-center">
-                              {open ? (
-                                <MinusIcon
-                                  className="block h-6 w-6 text-indigo-400 group-hover:text-indigo-500"
-                                  aria-hidden="true"
-                                />
-                              ) : (
-                                <PlusIcon
-                                  className="block h-6 w-6 text-gray-400 group-hover:text-gray-500"
-                                  aria-hidden="true"
-                                />
-                              )}
-                            </span>
-                          </Disclosure.Button>
-                        </h3>
-                        <Disclosure.Panel
-                          as="div"
-                          className="prose prose-sm pb-6"
-                        >
-                          <ul role="list">{detail.items}</ul>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                ))}
-              </div>
-            </section>
           </div>
         </div>
       </div>
