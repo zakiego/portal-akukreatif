caf99dc888cd6af96cffa9f4df60e7020614932c
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-09-28 05:07:36 +0800

    feat: remove subtitle hero (#7)

--- src/app/(home)/hero.tsx
+++ src/app/(home)/hero.tsx
@@ -33,11 +33,11 @@ export default async function Hero() {
                   di Kalimantan Selatan
                 </Balancer>
               </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
+              {/* <p className="mt-6 text-lg leading-8 text-gray-600">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
                 lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
                 fugiat aliqua.
-              </p>
+              </p> */}
               {/* <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
                   href="#"
