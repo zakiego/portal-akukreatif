329c1effe456850109c061614aed6ee0447e273e
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-10-03 17:21:53 +0800

    feat: resize logo

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -24,8 +24,8 @@ export default function Navbar() {
       >
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <div className="flex justify-center items-center w-20">
+            <span className="sr-only">Akukreatif</span>
+            <div className="flex justify-center items-center w-28">
               <Image src={Logo} alt="Logo" className="object-cover" />
             </div>
           </Link>
@@ -67,7 +67,7 @@ export default function Navbar() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Akukreatif</span>
               <div className="flex justify-center items-center w-20">
                 <Image src={Logo} alt="Logo" className="object-cover" />
               </div>
