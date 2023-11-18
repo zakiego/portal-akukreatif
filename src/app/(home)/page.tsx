75fdc2a95b598f318f4c43b8cade3d860d8b8060
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-11-18 09:55:43 +0800

    Add maintenance page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <Hero />
-      <Products />
+      {/* <Hero />
+      <Products /> */}
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/ui/navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/ui/footer";
+import Maintenance from "@/app/maintenance";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} antialiased`}>
         <div className="min-h-screen flex flex-col">
           <Navbar />
           <div className="flex-grow mt-32 mx-auto max-w-7xl px-6 pb-8 lg:px-8">
-            {children}
+            {/* {children} */}
+            <Maintenance />
           </div>
           <Footer />
         </div>
